028a7866a6d585c92d8045412bacdb1b3a557850
Alex <alex.adaumi@gmail.com>
2019-12-13 17:41:43 -0300

    cards with ramdom img

---
+++ src/components/Lorems.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container class="mx-auto" v-if="photos">
+    <v-row>
+      <v-col cols="12">
+        <v-row justify="center">
+          <v-btn @click="random">Random</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="12" md="6" lg="4" xl="3" v-for="photo in photos" :key="photo.id">
+        <v-card class="mx-auto" max-width="400">
+          {{photo}}
+          <v-img :src="photo.download_url"></v-img>
+          <v-card-actions>
+            <v-card-title>By {{photo.author}}</v-card-title>
+            <v-spacer></v-spacer>
+            <a :href="photo.download_url" class="text-decoration">
+              <v-btn icon>
+                <v-icon>mdi-download</v-icon>
+              </v-btn>
+            </a>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      page: 1,
+      photos: false,
+      picsum: ""
+    };
+  },
+  created() {
+    axios
+      .get("https://picsum.photos/v2/list?page=" + this.page + "&limit=100")
+      .then(response => {
+        this.photos = response.data;
+      })
+      .catch(() => {});
+  },
+  methods: {
+    random() {
+      axios.get;
+      this.page = Math.floor(Math.random() * 11);
+    }
+  }
+};
+</script>
+
+<style>
+.text-decoration {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar dark color="brown" flat elevation="2">
+    <div class="flex-grow-1"></div>
+    <v-toolbar-title class="text-center font-weight-medium display-1">Just a sample of Lorem Picsum</v-toolbar-title>
+    <div class="flex-grow-1"></div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
